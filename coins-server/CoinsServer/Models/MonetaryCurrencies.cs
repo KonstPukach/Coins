1ae1e667f78e0e3224a651404ade7cf14836d863
Yury Svistun <usv2001@yandex.ru>
2021-12-04 13:13:03 +0300

    Backend: News API

--- coins-server/CoinsServer/Models/MonetaryCurrencies.cs
+++ coins-server/CoinsServer/Models/MonetaryCurrencies.cs
@@ -4,33 +4,19 @@ using System.Runtime.Serialization;
 
 namespace CoinsServer.Models
 {
-    /// <summary>
-    /// MonetaryCurrencies model
-    /// </summary>
+
     [DataContract]
     public class MonetaryCurrencies
     {
-        /// <summary>
-        /// Success
-        /// </summary>
         [DataMember(Name = "success")]
         public bool Success { get; set; }
 
-        /// <summary>
-        /// Base
-        /// </summary>
         [DataMember(Name = "base")]
         public string Base { get; set; }
 
-        /// <summary>
-        /// Date
-        /// </summary>
         [DataMember(Name = "date")]
         public DateTime Date { get; set; }
 
-        /// <summary>
-        /// Rates
-        /// </summary>
         [DataMember(Name = "rates")]
         public Dictionary<string, decimal> Rates { get; set; }
     }
